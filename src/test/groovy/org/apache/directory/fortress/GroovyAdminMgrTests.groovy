125704741d2bdf14a7ffa4ac582dba68f8feaed9
Shawn McKinney <smckinney@symas.com>
2020-04-13 12:57:31 -0500

    more consts

--- src/test/groovy/org/apache/directory/fortress/GroovyAdminMgrTests.groovy
+++ src/test/groovy/org/apache/directory/fortress/GroovyAdminMgrTests.groovy
@@ -25,7 +25,7 @@ class GroovyAdminMgrTests
 
         // Use String constants for operation and entity names for readability.  Of course we could've used them for the actual values too.
         //         Operation      Entity                Attribute Map
-        admin.edit( Ids.ADD,    Ids.ORGUNIT,        name: 'Default', type: 'USER' )
+        admin.edit( Ids.ADD,    Ids.ORGUNIT,        name: TIds.DEFAULT, type: 'USER' )
         admin.edit( Ids.ADD,    Ids.ROLE,           name: TIds.WASHER)
         admin.edit( Ids.ENABLE, Ids.ROLECONSTRAINT, id: TIds.WASHER, key: 'locale' )
         admin.edit( Ids.ADD,    Ids.ROLE,           name: TIds.TELLER)
@@ -33,7 +33,7 @@ class GroovyAdminMgrTests
         admin.edit( Ids.ENABLE, Ids.ROLECONSTRAINT, id: TIds.TELLER, key: 'strength'  )
         admin.edit( Ids.ADD,    Ids.SDSET,          type: 'DYNAMIC', name: 'Bankers', members: [TIds.TELLER, TIds.WASHER], cardinality: '2', description: 'Ducks in a row' )
 
-        admin.edit( Ids.ADD, Ids.USER,              userId: 'huey', password: 'password', ou: 'Default', description: 'Groovy Test' )
+        admin.edit( Ids.ADD, Ids.USER,              userId: 'huey', password: 'password', ou: TIds.DEFAULT, description: 'Groovy Test' )
         admin.edit( Ids.ADD, Ids.USERROLE,          userId: 'huey', name: TIds.TELLER)
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'huey', id: TIds.TELLER, key: 'locale', value: 'East'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'huey', id: TIds.TELLER, key: 'strength', value: '2FA'  )
@@ -41,7 +41,7 @@ class GroovyAdminMgrTests
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'huey', id: TIds.WASHER, key: 'locale', value: 'North'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'huey', id: TIds.WASHER, key: 'locale', value: 'South'  )
 
-        admin.edit( Ids.ADD, Ids.USER,              userId: 'dewey', password: 'password', ou: 'Default', description: 'Groovy Test' )
+        admin.edit( Ids.ADD, Ids.USER,              userId: 'dewey', password: 'password', ou: TIds.DEFAULT, description: 'Groovy Test' )
         admin.edit( Ids.ADD, Ids.USERROLE,          userId: 'dewey', name: TIds.TELLER)
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'dewey', id: TIds.TELLER, key: 'locale', value: 'North'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'dewey', id: TIds.TELLER, key: 'strength', value: '2FA'  )
@@ -49,7 +49,7 @@ class GroovyAdminMgrTests
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'dewey', id: TIds.WASHER, key: 'locale', value: 'East'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'dewey', id: TIds.WASHER, key: 'locale', value: 'South'  )
 
-        admin.edit( Ids.ADD, Ids.USER,              userId: 'louie', password: 'password', ou: 'Default', description: 'Groovy Test' )
+        admin.edit( Ids.ADD, Ids.USER,              userId: 'louie', password: 'password', ou: TIds.DEFAULT, description: 'Groovy Test' )
         admin.edit( Ids.ADD, Ids.USERROLE,          userId: 'louie', name: TIds.TELLER)
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'louie', id: TIds.TELLER, key: 'locale', value: 'South'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'louie', id: TIds.TELLER, key: 'strength', value: '2FA'  )
@@ -57,8 +57,8 @@ class GroovyAdminMgrTests
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'louie', id: TIds.WASHER, key: 'locale', value: 'North'  )
         admin.edit( Ids.ADD, Ids.ROLECONSTRAINT,    userId: 'louie', id: TIds.WASHER, key: 'locale', value: 'East'  )
 
-        admin.edit( Ids.ADD, Ids.ORGUNIT,           name: 'Default', type: 'PERM' )
-        admin.edit( Ids.ADD, Ids.PERMOBJ,           objName: 'MONEY', ou: 'Default' )
+        admin.edit( Ids.ADD, Ids.ORGUNIT,           name: TIds.DEFAULT, type: 'PERM' )
+        admin.edit( Ids.ADD, Ids.PERMOBJ,           objName: 'MONEY', ou: TIds.DEFAULT )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'MONEY', opName: 'dry' )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'MONEY', opName: 'rinse' )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'MONEY', opName: 'soak' )
@@ -67,7 +67,7 @@ class GroovyAdminMgrTests
         admin.edit( Ids.ADD, Ids.PERMGRANT,         roleNm: TIds.WASHER, objName: 'MONEY', opName: 'rinse' )
         admin.edit( Ids.ADD, Ids.PERMGRANT,         roleNm: TIds.WASHER, objName: 'MONEY', opName: 'soak' )
 
-        admin.edit( Ids.ADD, Ids.PERMOBJ,           objName: 'ACCT', ou: 'Default' )
+        admin.edit( Ids.ADD, Ids.PERMOBJ,           objName: 'ACCT', ou: TIds.DEFAULT )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'ACCT', opName: 'deposit' )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'ACCT', opName: 'inquiry' )
         admin.edit( Ids.ADD, Ids.PERMISSION,        objName: 'ACCT', opName: 'withdrawal' )
@@ -86,12 +86,12 @@ class GroovyAdminMgrTests
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'MONEY', opName: 'dry' )
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'MONEY', opName: 'rinse' )
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'MONEY', opName: 'soak' )
-        admin.edit( Ids.DELETE, Ids.PERMOBJ,    objName: 'MONEY', ou: 'Default' )
+        admin.edit( Ids.DELETE, Ids.PERMOBJ,    objName: 'MONEY', ou: TIds.DEFAULT )
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'ACCT', opName: 'deposit' )
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'ACCT', opName: 'inquiry' )
         admin.edit( Ids.DELETE, Ids.PERMISSION, objName: 'ACCT', opName: 'withdrawal' )
-        admin.edit( Ids.DELETE, Ids.PERMOBJ,    objName: 'ACCT', ou: 'Default' )
-        admin.edit( Ids.DELETE, Ids.ORGUNIT,    name: 'Default', type: 'PERM' )
+        admin.edit( Ids.DELETE, Ids.PERMOBJ,    objName: 'ACCT', ou: TIds.DEFAULT )
+        admin.edit( Ids.DELETE, Ids.ORGUNIT,    name: TIds.DEFAULT, type: 'PERM' )
 
         admin.edit( Ids.DELETE, Ids.SDSET,          type: 'DYNAMIC', name: 'Bankers' )
         admin.edit( Ids.DISABLE,Ids.ROLECONSTRAINT, id: TIds.WASHER, key: 'locale',  )
@@ -103,7 +103,7 @@ class GroovyAdminMgrTests
         admin.edit( Ids.DELETE, Ids.USER,       userId: 'huey' )
         admin.edit( Ids.DELETE, Ids.USER,       userId: 'dewey' )
         admin.edit( Ids.DELETE, Ids.USER,       userId: 'louie' )
-        admin.edit( Ids.DELETE, Ids.ORGUNIT,    name: 'Default', type: 'USER' )
+        admin.edit( Ids.DELETE, Ids.ORGUNIT,    name: TIds.DEFAULT, type: 'USER' )
 
         //admin.end()
     }
--- src/test/groovy/org/apache/directory/fortress/TIds.groovy
+++ src/test/groovy/org/apache/directory/fortress/TIds.groovy
@@ -4,4 +4,5 @@ class TIds
 {
     public static final String WASHER = 'Washer'
     public static final String TELLER = 'Teller'
+    public static final String DEFAULT = 'Default'
 }
