a5ae3e982f2013458b5a46a1aa85f049e3874565
Shawn McKinney <smckinney@symas.com>
2020-04-13 09:47:56 -0500

    javadoc

--- src/main/groovy/org/apache/directory/fortress/GroovyAccessMgr.groovy
+++ src/main/groovy/org/apache/directory/fortress/GroovyAccessMgr.groovy
@@ -7,6 +7,11 @@ import org.apache.directory.fortress.core.model.RoleConstraint
 import org.apache.directory.fortress.core.model.Session
 import org.apache.directory.fortress.core.model.User
 
+/**
+ * Wrapper for Apache Fortress AccessMgr.  This class is not thread safe.
+ * To begin an rbac session, call the start method.  If successful, the session will be set as an instance variable on this class.
+ * Subsequently the canDo method may be called, which will use the session member.  Finally, call the end method to clear out the session and error data.
+ */
 class GroovyAccessMgr
 {
     Session session
@@ -18,12 +23,22 @@ class GroovyAccessMgr
         reason = null
     }
 
+    /**
+     * Apache Fortress createSession wrapper.
+     *
+     * @param options are key value pairs used during the signon including: userId, password, roles and constraints.
+     * The password is optional, if not passed in, will be a 'trusted' session.
+     * roles (optional) contains a list of names to be considered for activation. If set, fortress only activates those in the list.
+     * Otherwise, will attempt activation all of the roles assigned to the user.
+     * Constraints (constraints) are used during the role activation phase, for processing dynamic attributes, e.g. locale.
+     * @return boolean true if successful, otherwise false, with reason set.
+     */
     boolean start( Map options=[:] )
     {
         boolean rc = false
         initialize()
         String userId, password=null
-        println "start-> userId:$userId password:$password constraints:$options"
+        println "start-> options:$options"
         List<RoleConstraint> constraints;
         String[] roles;
         for (entry in options)
@@ -75,12 +90,15 @@ class GroovyAccessMgr
     }
 
 
-    def end( )
-    {
-        session = null
-        reason = null
-    }
-
+    /**
+     * Maps to the Fortress checkAccess method.  It is passed object, operation and optionally an objId.
+     * It will determine if the user has a role activated in their session that has the passed permission.
+     *
+     * @param object name of resource being checked.  Like an account or customer record.
+     * @param operation being performed like add, update, delete, etc.
+     * @param objId is the identity of the object, e.g.  Account '123'.
+     * @return boolean true if allowed, otherwise false.
+     */
     boolean canDo ( String object, String operation, String objId = null )
     {
         boolean rc = false
@@ -108,6 +126,12 @@ class GroovyAccessMgr
         return rc
     }
 
+    /**
+     * Determine if the session member variable contains the role being passed in here.
+     *
+     * @param roleNm required.
+     * @return boolean true if role is found, otherwise false.
+     */
     boolean inRole ( String roleNm )
     {
         boolean rc = false
@@ -127,5 +151,16 @@ class GroovyAccessMgr
             reason = 'session is null'
         }
         return rc
+    }
+
+    /**
+     * Clears out the session and error data.
+     *
+     * @return
+     */
+    def end( )
+    {
+        session = null
+        reason = null
     }
 }
